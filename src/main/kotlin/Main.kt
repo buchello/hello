a46dba61d650eb7c2c28f1e7b3b6347ae30ee504
buchello <buchello818@gmail.com>
2023-08-16 16:19:22 +0300

    переделал

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,7 @@
 import kotlin.random.Random
 fun main() {
-    val rand = Random(1 )
-    val arraySize = 10
-    val array = IntArray(arraySize)
-    for (i in 0 until arraySize) {
-        array[i] = rand.nextInt(37)
-    }
-    println(array.contentToString())
+    val randomNumb = List(8) {Random.nextInt(1, 36)}
+    println(randomNumb)
 
 }
 
