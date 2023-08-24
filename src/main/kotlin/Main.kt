5116a69fbebcb5e6c17505f8f176316b9e068366
buchello <142268148+buchello@users.noreply.github.com>
2023-08-24 14:16:38 +0300

    Рандом уникальный

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,22 @@
 import kotlin.random.Random
-fun main() {
-    val randomNumb = List(8) {Random.nextInt(1, 36)}
-    println(randomNumb)
 
-}
+val listCirculation: MutableList<Circulation> = mutableListOf()
+fun main() {
+    for (i in 1..100) {
+        val uniqueNumbers = mutableListOf<Int>()
+        while (uniqueNumbers.size < 8) {
+            val list = Random.nextInt(1, 36)
+            if (list !in uniqueNumbers) {
+                uniqueNumbers.add(list)
+            }
+        }
+        listCirculation.add(Circulation(i, uniqueNumbers))
+    }
+        listCirculation.forEach {
+            println("${it.id}${it.listInt}")
+        }
+    }
+
+class Circulation(val id: Int, val listInt: MutableList<Int>)
+
 
